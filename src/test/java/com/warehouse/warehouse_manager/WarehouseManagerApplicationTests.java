6d48ea03dac6f7283ba22cac9ed5e7cbb71e0809
2berkullezosamasonches <maxlungul79@gmail.com>
2025-12-27 15:35:23 +0300

    Update WarehouseManagerApplicationTests.java

--- src/test/java/com/warehouse/warehouse_manager/WarehouseManagerApplicationTests.java
+++ src/test/java/com/warehouse/warehouse_manager/WarehouseManagerApplicationTests.java
@@ -3,7 +3,7 @@ package com.warehouse.warehouse_manager;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class WarehouseManagerApplicationTests {
 
 	@Test
